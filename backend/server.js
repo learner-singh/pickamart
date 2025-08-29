391f5ebd8b35504397285309bacd723097503c22
Kishan <kishan.singh@tailwnd.com>
2025-08-29 10:51:34 +0530

    fix: __dirName issue resolved

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,8 @@ app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirName = path.resolve(); // Set __dirname to current directory
-app.use("/uploads", express.static(path.join(__dirName, "/uploads")));
+const __dirname = path.resolve(); // Set __dirname to current directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   // set static folder
